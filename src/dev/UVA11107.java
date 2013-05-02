09cadc4b5268942ad7e91b8045fdc219b8b21a71
YaqiangCao <caoyaqiang0410@gmail.com>
2013-05-02 13:30:33 +0800

    commit

--- src/dev/UVA11107.java
+++ src/dev/UVA11107.java
@@ -5,36 +5,33 @@
  * Created Date:Apr 30, 20137:49:54 PM
  * Modified Date: Apr 30, 20137:49:54 PM
  * Copyright (c) 2013, caoyaqiang0410@gmail.com All Rights Reserved.
- * 主要参考资料:后缀数组——处理字符串的有力工具
  */
 package dev;
 
+import java.lang.*;
+import java.util.*;
+import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.Arrays;
 
 
 class SuffixArray {
     final String[] suffixes;
-    final int[] height;
     final int N;
 
     public SuffixArray(String s) {
         N = s.length();
         suffixes = new String[N];
-        height=new int[N];
         for (int i = 0; i < N; i++)
             suffixes[i] = s.substring(i);
         Arrays.sort(suffixes);
-        for( int i=1;i<N;i++ ){
-           height[ i ] =lcp( i );
-        }
     }
 
     // ith sorted suffix
     public String select(int i) { return suffixes[i]; }
     
-   // length of longest common prefix of s and t
-    private static int lcp(String s, String t) {
+    // length of longest common prefix of s and t
+    public static int lcp(String s, String t) {
         int N = Math.min(s.length(), t.length());
         for (int i = 0; i < N; i++)
             if (s.charAt(i) != t.charAt(i)) return i;
@@ -49,6 +46,7 @@ class SuffixArray {
 
 
 
+
 public class UVA11107 {
 	public static void main(String[] args) {
 		Scanner cin=new Scanner(System.in);
@@ -56,57 +54,70 @@ public class UVA11107 {
 			int n=Integer.parseInt(cin.next());
 			if(n==0){
 				return;
+			}else{
+				int[] ls=new int[n+1];
+				ls[0]=0;
+				String line="";
+				for(int i=1;i<=n;i++){
+					String tmp=cin.next();
+					line+=tmp+i;
+					ls[i]=ls[i-1]+1+tmp.length();
+				}
+				findSub(line,n,ls);
 			}
-			String line="";
-			for(int i=1;i<=n;i++){
-                line+=cin.next()+i;
-			}
-			System.out.println(line);
-			findSub(line,n);
 		}
 	}
 
-	private static void findSub(String line, int n) {
+	private static void findSub(String line, int n, int[] ls) {
         SuffixArray suffix = new SuffixArray(line);
         //the length of repeat substring
         int mm=1;
-        //the repeat time of the substring,only output the repeate time >=2
-        int r=0;
-        //the repeat string/strings
-        String ss="";
+        //the repeat substring
+        ArrayList<String> rs=new ArrayList<String>(  );
         //find the longest common substring
         for(int i=1;i<suffix.N;i++){
-            int h=suffix.height[ i ];
+            int h=suffix.lcp(i);
+            if( h<mm ){
+                continue;
+            }
+            String tmps=suffix.select( i ).substring( 0,h );
+            //the repeat time of the tmp substring
+            int tmpr=0;
+            int j=0;
+            //find the repeat time
+            while( true ){
+               	j=line.indexOf(tmps,j);
+               	if(j==-1){
+               		break;
+               	}
+               	tmpr+=1;
+               	//change the start index for substring search to next line
+               	for(int k=1;k<ls.length;k++){
+               		if(j<ls[k]){
+               			j=ls[k];
+               			break;
+               		}
+               	}
+               	if(j>=line.length()){
+               		break;
+               	}
+            }
+            if( tmpr<=n/2 ){
+                continue;
+            }
             if( h>mm ){
-                mm=h;
+                rs.clear(  );
             }
+            rs.add( tmps );
+            mm=h;
         }
-        //for every longest common substring, check their repeat time
-        for(int i=1;i<suffix.N;i++){
-            int h=suffix.height[ i ];
-            if( h==mm ){
-                String tmps=suffix.select( i ).substring( 0,h );
-              //the repeat time of the tmp substring
-                int tmpr=0;
-                int j=0;
-                //find the repeat time
-                while( line.indexOf(tmps,j) !=-1 ){
-                	tmpr+=1;
-                	j=line.indexOf(tmps,j)+tmps.length(  );
-                	if (j>=line.length()){
-                		break;
-                	}
-                }
-                if( tmpr>n/2 ){
-                   ss+=tmps+"\n";
-                }
-            }
-            
-        }
-        if( ss.length(  )==0 ){
+        if( rs.size()==0 ){
             System.out.printf( "?\n\n");    
         }else{
-            System.out.printf( "%s\n",ss );
+        	for(String r:rs){
+        		System.out.printf( "%s\n",r);
+        	}
+        	System.out.printf( "\n");
         }
     }
 
